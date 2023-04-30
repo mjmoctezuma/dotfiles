d02b92f4ee8be44c25cc72d140bb9f5ce7dda005
MoisesJuarez <97188382+mjmoctezuma@users.noreply.github.com>
2023-04-30 11:03:33 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,3 +21,4 @@ alias yh='yt-dlp --continue --no-check-certificate --format=bestvideo+bestaudio 
 alias yd='yt-dlp --continue --no-check-certificate --format=bestvideo+bestaudio --exec="ffmpeg -i {} -c:v prores_ks -profile:v 1 -vf fps=25/1 -pix_fmt yuv422p -c:a pcm_s16le {}.mov && rm {}"'
 alias ya='yt-dlp --continue --no-check-certificate --format=bestaudio -x --audio-format wav'
 alias pip_upgrade_all="pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
+alias vim='nvim'
